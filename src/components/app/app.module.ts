f6944f21cecd4f4dae98f7d6cd020fc9ea2baaae
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-09-11 23:25:12 +0300

    deploy ready

--- src/components/app/app.module.ts
+++ src/components/app/app.module.ts
@@ -6,30 +6,34 @@ import { AuthModule } from '../auth/auth.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { PublicationModule } from '../publication/publication.module';
-import { FileModule } from '../photo/file.module';
+import { FileModule } from '../file/file.module';
 import { MapModule } from '../map/map.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { CityModule } from '../city/city.module';
+import { MemorialModule } from '../memorial/memorial.module';
+import { MemorialTypeModule } from '../memorialType/memorialType.module';
 
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../../..', 'client/public/img'),
+      rootPath: join(__dirname, '../../../..', 'client/public/img'),
       exclude: ['/api/(.*)'],
       serveRoot: '/img',
     }),
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../../..', 'client'),
-      exclude: ['/api*'],
+      rootPath: join(__dirname, '../../../..', 'client/dist'),
+      exclude: ['/api/(.*)'],
     }),
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     MapModule,
     FileModule,
     PublicationModule,
     AuthModule,
+    CityModule,
+    MemorialModule,
+    MemorialTypeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
